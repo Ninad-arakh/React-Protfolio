1ac73adb6ee8068bfcac0f74206323c87dfcd707
Ninad-arakh <arakhninad@gmail.com>
2025-01-31 17:03:49 +0530

    Update sesClient.js

--- src/Utils/sesClient.js
+++ src/Utils/sesClient.js
@@ -5,8 +5,8 @@ const REGION = "ap-south-1";
 export const sesClient = new SESClient({
   region: REGION,
   credentials: {
-    accessKeyId: AKIAST6S7CBWCMZ3SPE2,
-    secretAccessKey: 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,
+    accessKeyId: "AKIAST6S7CBWCMZ3SPE2",
+    secretAccessKey: "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",
   },
 });
 
